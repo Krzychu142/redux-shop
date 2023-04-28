d6362af9cde7512aeb3403c857564288ec581618
Krzysztof Radzięta <kustosz142@gmail.com>
2023-04-28 08:31:44 +0200

    feat(episod-3): finish

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,16 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
         <Route exact path="/cart" element={<Cart />} />
+        <Route exact path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/notfound.css'
+
+const NotFound = () => {
+  return (
+    <div className='not-found'>
+        <h2>404</h2>
+        <p>Page not found</p>
+    </div>
+  )
+}
+
+export default NotFound
