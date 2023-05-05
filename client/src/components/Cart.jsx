4fca2b2dae211efd50e302783b4872326885afac
Krzysztof Radzięta <kustosz142@gmail.com>
2023-05-05 10:11:23 +0200

    feat(episod-14): styled desktop wersion

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -6,14 +6,14 @@ import CartItem from './CartItem';
 
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
-  console.log(cart);
 
   return (
     <main className="cart-container">
+      <h2>Shopping Cart</h2>
       {cart.cartItems.length === 0 ? (
         <div className="cart--empty">
           <p>Your cart is empty</p>
-          <div className="cart__start-shopping">
+          <div className="start-shopping">
             <Link to="/">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -59,26 +59,28 @@ const Cart = () => {
             <div className="cart__checkout">
               <div className="cart__subtotal">
                 <span>Subtotal</span>
-                <span className="cart__subtotal-price"></span>
+                <span className="cart__subtotal-price">$0</span>
               </div>
               <p>Taxes and shipping calculated at checkout</p>
               <button className="checkout">Checkout</button>
-              <Link to="/">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  className="bi bi-arrow-return-left"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
-                  />
-                </svg>
-                <span>Continue shopping</span>
-              </Link>
+              <div className="continue-shopping">
+                <Link to="/">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    className="bi bi-arrow-return-left"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M14.5 1.5a.5.5 0 0 1 .5.5v4.8a2.5 2.5 0 0 1-2.5 2.5H2.707l3.347 3.346a.5.5 0 0 1-.708.708l-4.2-4.2a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 8.3H12.5A1.5 1.5 0 0 0 14 6.8V2a.5.5 0 0 1 .5-.5z"
+                    />
+                  </svg>
+                  <span>Continue shopping</span>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,25 +1,27 @@
 import React from 'react';
+import '../styles/cart.css';
 
 const CartItem = ({ image, name, desc, price, quantity }) => {
   return (
     <div className="cart__item">
       <div className="cart-product">
         <img src={image} alt={name} />
-        <div className="cart-product__details">
+        <div>
           <h3>{name}</h3>
           <p>{desc}</p>
+          <button>Remove</button>
         </div>
-        <div className="cart-product__price">
-          <span>${price}</span>
-        </div>
-        <div className="cart-product__quantity">
-          <button>-</button>
-          <span className="count">{quantity}</span>
-          <button>+</button>
-        </div>
-        <div className="cart-product__total">
-          <span>${price * quantity}</span>
-        </div>
+      </div>
+      <div className="cart-product__price">
+        <span>${price}</span>
+      </div>
+      <div className="cart-product__quantity">
+        <button>-</button>
+        <span className="count">{quantity}</span>
+        <button>+</button>
+      </div>
+      <div className="cart-product__total">
+        <span>${price * quantity}</span>
       </div>
     </div>
   );
