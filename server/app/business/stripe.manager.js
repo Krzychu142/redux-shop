0c737d0266ddda5c11d4101ac97f9cfe08c77bb8
Krzysztof Radzięta <kustosz142@gmail.com>
2023-05-13 21:54:54 +0200

    feat(stripe-integration): tested backedn

--- server/app/business/stripe.manager.js
+++ server/app/business/stripe.manager.js
@@ -2,25 +2,26 @@ const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
 
 const createCheckoutSession = async (res, req, data) => {
-	const session = await stripe.checkout.sessions.create({
-		line_items: [
-			{
-				price_data: {
-					currency: "usd",
-					product_data: {
-						name: "T-shirt",
-					},
-					unit_amount: 2000,
-				},
-				quantity: 1,
-			},
-		],
-		mode: "payment",
-		success_url: `${process.env.CLIENT_URL}/checkout-success`,
-		cancel_url: `${process.env.CLIENT_URL}/cart`,
-	});
-
-	res.send({ url: session.url });
+  const items = req.cartItems;
+  const line_items = items.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100,
+      },
+      quantity: item.cartQuantity,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    line_items: line_items,
+    mode: "payment",
+    success_url: `${process.env.CLIENT_URL}/checkout-success`,
+    cancel_url: `${process.env.CLIENT_URL}/cart`,
+  });
+  res.send({ url: session.url });
 };
 
 module.exports = { createCheckoutSession };
