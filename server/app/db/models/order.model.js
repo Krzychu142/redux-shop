a11000f35ea3607c6ee302132987d0f5f7853d0f
Krzysztof Radzięta <kustosz142@gmail.com>
2023-05-17 21:51:13 +0200

    feat(stripe-integration): created order model

---
+++ server/app/db/models/order.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        id: { type: String, required: true },
+        name: { type: String },
+        brand: { type: String },
+        desc: { type: String },
+        price: { type: String },
+        image: { type: String },
+        cartQuantity: { type: Number },
+      },
+    ],
+    subtotal: { type: Number, required: true },
+    // total is subtotal + tax + shipping
+    total: { type: Number, required: true },
+    shipping: { type: Object, required: true },
+    delivery_status: { type: String, default: "pending" },
+    payment_status: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
